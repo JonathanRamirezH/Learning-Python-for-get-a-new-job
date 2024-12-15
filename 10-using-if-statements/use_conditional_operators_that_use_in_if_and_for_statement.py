3d3ff9ba13d37a2a0c93e237c385dfdad1493fe6
Jonathan Ramirez <jonathan176h@gmail.com>
2024-12-15 07:40:09 -0600

    actualizacion dentro de repositoria de aprendizaje en Python Se aprendio a como usar los condicionales dentro de una ciclo for y como ejecutar un bloque de codigo haciendo uso de condicionales

---
+++ 10-using-if-statements/use_conditional_operators_that_use_in_if_and_for_statement.py
@@ -0,0 +1,11 @@
+#dentro de Python hay operadores que se encargan de poder 
+#hacer declaraciones mas complejas
+print('1==1',1==1)#IGUAL A
+print('1!=1',1!=1)#DIFERENTE A
+print('1<2',1<2)#MENOR QUE
+print('1>2',1>2)#MAYOR QUE
+print('1<=1',1<=1)#MENOR O IGUAL QUE
+print('2>=3',2>=3)#MAYOR O IGUAL QUE
+#PARA HACER CONDICIONALES MAS COMPLEJAS SE HACE USA DE 
+#TRES PALABRAS RESERVADAS (AND, OR Y NOT)
+print('3>2 and 3<6',3>2 and 3<6)
